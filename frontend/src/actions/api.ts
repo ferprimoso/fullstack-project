4af061e01dbe71c531568e55af43dad1763f744c
Luiz Fernando Campos Jose Filho <ferprimoso@gmail.com>
2024-02-13 07:26:52 -0300

    Fix api enviroment

--- frontend/src/actions/api.ts
+++ frontend/src/actions/api.ts
@@ -2,8 +2,6 @@ let apiKey: undefined | string
 
 if (process.env.NODE_ENV === 'production') {
   apiKey = process.env.API_PRODUCTION
-} else {
-  apiKey = 'http://pc-backend:3001'
 }
 
 export default apiKey
