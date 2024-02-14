21eb162d248e36cf3d6171f198591a258f529e99
Luiz Fernando Campos Jose Filho <ferprimoso@gmail.com>
2024-02-14 07:27:31 -0300

    Fix api not working in development

--- frontend/src/actions/api.ts
+++ frontend/src/actions/api.ts
@@ -2,6 +2,8 @@ let apiKey: undefined | string
 
 if (process.env.NODE_ENV === 'production') {
   apiKey = process.env.API_PRODUCTION
+} else {
+  apiKey = process.env.NEXT_PUBLIC_NETWORK_API_URL
 }
 
 export default apiKey
